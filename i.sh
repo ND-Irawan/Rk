211c23cdbc0b043797bd5079d08fc0912d9450d8
Ndra <80077633+ND-Irawan@users.noreply.github.com>
2025-02-01 11:58:10 +0700

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#set -e
-## Copy this script inside the kernel directory
 CLANGDIR="/workspace/build/clang"
 export KBUILD_BUILD_USER=Ndrα
 export KBUILD_BUILD_HOST=ND-Irαwαn
@@ -88,23 +86,29 @@ fi
 
 # kernel-Compilation
 
-make $KERNEL_DEFCONFIG O=out CC=clang
-make -j$(nproc --all) O=out \
-  CC=clang \
-  ARCH=arm64 \
-  CROSS_COMPILE=aarch64-linux-gnu- \
-  NM=llvm-nm \
-  OBJDUMP=llvm-objdump \
-  STRIP=llvm-strip
+make O=out ARCH=arm64 alioth_defconfig
+nd () {
+make -j$(nproc --all) O=out LLVM=1 LLVM_IAS=1 \
+ARCH=arm64 \
+CC=clang \
+LD=ld.lld \
+AR=llvm-ar \
+AS=llvm-as \
+NM=llvm-nm \
+STRIP=llvm-strip \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+READELF=llvm-readelf \
+HOSTCC=clang \
+HOSTCXX=clang++ \
+HOSTAR=llvm-ar \
+HOSTLD=ld.lld \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
 
-TIME="$(date "+%Y%m%d-%H%M%S")"
-if [ $? -ne 0 ]
-then
-    echo "Build failed"
-else
-    echo "Build succesful"
-    cp out/arch/arm64/boot/dtbo.img out/outputs/Ndra/dtbo.img
-    cp out/arch/arm64/boot/Image.gz out/outputs/Ndra/Image.gz
-    cp out/arch/arm64/boot/dtb out/Ndra/dtb
-fi
-echo $TIME
+nd 2>&1 | tee -a compile.log
+
+# Revert changes back to the original state
+revert_modifications
+
